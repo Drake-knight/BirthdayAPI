801f2df515af6c2b44e678c318227d895ff86c8c
vishal <jakharvishalv@gmail.com>
2023-07-13 18:46:28 +0530

    Improved indentation

---
+++ module/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const controller = require('../controller/bdaycontroller');
+const PORT = 3000;
+
+var app = express();
+
+controller(app);
+
+app.listen(PORT , () =>{
+console.log(`Listening to port ${PORT}`);
+});
+
